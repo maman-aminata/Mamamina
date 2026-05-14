e15557dc3f39d4710303c3033b67df490a722a0b
Maman Aminata Niane <nianemamanaminata21@gmail.com>
2026-05-14 18:35:20 +0000

    modification projets.php

--- projets.php
+++ projets.php
@@ -4,19 +4,27 @@ require 'fonctions.php';
 $projets = [
     [
         'titre' => 'Projet Eclipse (Conversion / Exercices)',
-        'description' => 'Ce projet réalisé sur Eclipse consistait à effectuer des exercices de programmation, notamment des conversions et manipulations de données. L\'objectif était de mettre en pratique les notions de base en programmation, comme les variables, les opérations et l\'affichage des résultats. Ce travail m\'a permis de renforcer ma logique et de mieux comprendre le fonctionnement d\'un programme simple.',
+        'description' => 'Ce projet réalisé sur Eclipse consistait à effectuer des exercices de programmation, notamment des conversions et
+        manipulations de données. L’objectif était de mettre en pratique les notions de base en programmation, comme les
+        variables, les opérations et l’affichage des résultats. Ce travail m’a permis de renforcer ma logique et de mieux
+        comprendre le fonctionnement d’un programme simple.',
         'image' => 'images/projet1.png',
         'technologies' => ['Java', 'Eclipse']
     ],
     [
         'titre' => 'Projet MySQL (Création de table)',
-        'description' => 'Ce projet consiste à créer et manipuler une base de données avec MySQL, notamment à travers la création d\'une table. J\'ai utilisé des requêtes SQL comme CREATE TABLE pour définir la structure des données. Ce projet m\'a permis de comprendre les bases de la gestion de données et l\'organisation d\'une base de données relationnelle.',
+        'description' => ' Ce projet consiste à créer et manipuler une base de données avec MySQL, notamment à travers la création d’une table.
+        J’ai utilisé des requêtes SQL comme CREATE TABLE pour définir la structure des données. Ce projet m’a permis de
+        comprendre les bases de la gestion de données et l’organisation d’une base de données relationnelle.',
         'image' => 'images/projet3.jpeg',
         'technologies' => ['MySQL', 'SQL']
     ],
     [
         'titre' => 'Projet Arduino (Allumage de lampes)',
-        'description' => 'Ce projet consiste à programmer un système avec Arduino permettant d\'allumer et d\'éteindre des lampes. Il repose sur l\'utilisation de composants électroniques et la programmation du microcontrôleur pour contrôler les sorties selon des conditions définies. Ce projet m\'a permis de découvrir le fonctionnement des systèmes embarqués et d\'interagir avec du matériel électronique de manière concrète.',
+        'description' => 'Ce projet consiste à programmer un système avec Arduino permettant d’allumer et d’éteindre des lampes. Il repose sur
+        l’utilisation de composants électroniques et la programmation du microcontrôleur pour contrôler les sorties selon des
+        conditions définies. Ce projet m’a permis de découvrir le fonctionnement des systèmes embarqués et d’interagir avec du
+        matériel électronique de manière concrète..',
         'image' => 'images/projet4.png',
         'technologies' => ['Arduino']
     ]
