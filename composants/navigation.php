df20d0c0084ff6332791a34af464ec295341740a
Maman Aminata Niane <nianemamanaminata21@gmail.com>
2026-05-15 19:58:00 +0000

    modification

--- composants/navigation.php
+++ composants/navigation.php
@@ -1,11 +1,15 @@
 <?php
 $page_courante = basename($_SERVER['PHP_SELF']);
 ?>
-<nav>
-  <input type="text" class="search-bar" placeholder="Rechercher...">
-<a href='index.php' <?php if ($page_courante === 'index.php') echo 'class="actif"'; ?>>Accueil</a>
-<a href='about.php' <?php if ($page_courante === 'about.php') echo 'class="actif"'; ?>>À propos</a>
-<a href='projets.php' <?php if ($page_courante === 'projets.php') echo 'class="actif"'; ?>>Projets</a>
-<a href='contact.php' <?php if ($page_courante === 'contact.php') echo 'class="actif"'; ?>>Contact</a>
+<nav class="navbar">
+  <div class="nav-search">
+    <input type="text" class="search-bar" placeholder="Rechercher...">
+  </div>
+  <div class="nav-links">
+    <a href='index.php' <?php if ($page_courante === 'index.php') echo 'class="actif"'; ?>>Accueil</a>
+    <a href='about.php' <?php if ($page_courante === 'about.php') echo 'class="actif"'; ?>>À propos</a>
+    <a href='projets.php' <?php if ($page_courante === 'projets.php') echo 'class="actif"'; ?>>Projets</a>
+    <a href='contact.php' <?php if ($page_courante === 'contact.php') echo 'class="actif"'; ?>>Contact</a>
+  </div>
 </nav>
 
