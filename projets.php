7a8d663250c7ffad4665c5d3c538a315498e6356
Maman Aminata Niane <nianemamanaminata21@gmail.com>
2026-05-15 20:22:20 +0000

    modification

--- projets.php
+++ projets.php
@@ -62,7 +62,7 @@ if ($mot_cle !== '') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Projets</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="/style.css">
 </head>
 
 <body>
