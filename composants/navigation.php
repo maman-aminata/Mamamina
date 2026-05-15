9dc4c8e9ef499ceb5e4193ee1c650a92b354d5ca
Maman Aminata Niane <nianemamanaminata21@gmail.com>
2026-05-15 17:36:52 +0000

    modification

--- composants/navigation.php
+++ composants/navigation.php
@@ -7,13 +7,13 @@ $page_courante = basename($_SERVER['PHP_SELF']);
     <?php if ($page_courante === 'index.php') echo 'class="actif"'; ?>>
     Accueil
   </a>
-  <a href='projets.php'
-    <?php if ($page_courante === 'projets.php') echo 'class="actif"'; ?>>
-    Projets
-  </a>
   <a href='about.php'
     <?php if ($page_courante === 'about.php') echo 'class="actif"'; ?>>
     À propos
+  </a>
+  <a href='projets.php'
+    <?php if ($page_courante === 'projets.php') echo 'class="actif"'; ?>>
+    Projets
   </a>
   <a href='contact.php'
     <?php if ($page_courante === 'contact.php') echo 'class="actif"'; ?>>
