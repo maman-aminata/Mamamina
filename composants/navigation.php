6bb7fe5ce511b373b9130da0db6608816ae917fe
Maman Aminata Niane <nianemamanaminata21@gmail.com>
2026-05-15 21:18:39 +0000

    modification

--- composants/navigation.php
+++ composants/navigation.php
@@ -1,15 +1,17 @@
 <?php
 $page_courante = basename($_SERVER['PHP_SELF']);
 ?>
-<nav class="navbar">
+<header>
+    <h1>Mon Portfolio</h1>
   <div class="nav-search">
     <input type="text" class="search-bar" placeholder="Rechercher...">
   </div>
-  <div class="nav-links">
+  
+  <nav>
     <a href='index.php' <?php if ($page_courante === 'index.php') echo 'class="actif"'; ?>>Accueil</a>
     <a href='about.php' <?php if ($page_courante === 'about.php') echo 'class="actif"'; ?>>À propos</a>
     <a href='projets.php' <?php if ($page_courante === 'projets.php') echo 'class="actif"'; ?>>Projets</a>
     <a href='contact.php' <?php if ($page_courante === 'contact.php') echo 'class="actif"'; ?>>Contact</a>
-  </div>
-</nav>
+  </nav>
+</header>
 
